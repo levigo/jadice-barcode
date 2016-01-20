fdc3e354573b20abb518287ad085a3f3079f232a
Thomas Grammetbauer <t.grammetbauer@levigo.de>
2016-01-20 14:50:15 +0100

    format

--- engine/src/main/java/com/jadice/barcode/linear/tofi/TwoOfFiveChecksumVerifier.java
+++ engine/src/main/java/com/jadice/barcode/linear/tofi/TwoOfFiveChecksumVerifier.java
@@ -12,18 +12,22 @@ public class TwoOfFiveChecksumVerifier implements ChecksumVerifier {
   public boolean verifyChecksum(CodeString result) {
     final int[] codes = result.getCodes();
 
-    int calculatedChecksum = 0;
-    for (int i = 0; i < codes.length - 1; i++)
-      calculatedChecksum += codes[i] * (i % 2 != 0 ? 3 : 1);
-
+    int calculatedChecksum = calculateChecksum(codes);
     int checksum = codes[codes.length - 1];
-    
+
     // calculated checksum matches appended checksum
-    if (calculatedChecksum % 10 == checksum) {
+    if (calculatedChecksum == checksum) {
       result.setChecksumVerificationOK(true);
       return true;
     }
 
     return false;
+  }
+
+  private int calculateChecksum(int[] codes) {
+    int checksum = 0;
+    for (int i = 0; i < codes.length - 1; i++)
+      checksum += codes[i] * (i % 2 != 0 ? 3 : 1);
+    return checksum % 10;
   }
 }
